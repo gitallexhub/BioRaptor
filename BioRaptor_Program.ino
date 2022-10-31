5fa7fe989866f5096ff40bb5d76aa08605c43214
irelandmbrown <116670278+irelandmbrown@users.noreply.github.com>
2022-10-31 11:30:06 -0400

    Update BioRaptor_Program.ino
    Fix horizontal zeroing.

--- BioRaptor_Program.ino
+++ BioRaptor_Program.ino
@@ -34,19 +34,20 @@ Stepper LinearStepper  = Stepper(LinearMotorStepPerRev, 9, 10, 11, 12);
 Stepper OrbitalStepper = Stepper(OrbitalMotorStepPerRev, 13, 14, 15, 16);
 
   // Assign state variable to user input buttons to define pushed or not pushed, limit switches closed or not open, these are not constant.
-int Linear_ButtonState    = 0;
-int Orbital_ButtonState   = 0;
-int D_1_ButtonState       = 0;
-int D_2_ButtonState       = 0;
-int D_3_ButtonState       = 0;
-int D_4_ButtonState       = 0; 
-int Limit_Switch_OneState = 0;
-int Limit_Switch_TwoState = 0;
-int Emergency_StopState   = 0;
+int Linear_ButtonState        = 0;
+int Orbital_ButtonState       = 0;
+int D_1_ButtonState           = 0;
+int D_2_ButtonState           = 0;
+int D_3_ButtonState           = 0;
+int D_4_ButtonState           = 0; 
+int Limit_Switch_OneState     = 0;
+int Limit_Switch_TwoState     = 0;
+int Emergency_StopState       = 0;
+int Speed_PontentiometerState = 0;
 
-  // Define step count.
-int LinearMotorSteps      = 0;
-int OrbitalMotorSteps     = 0;
+  // Define step count. Not sure how this works.
+// int LinearMotorSteps      = 0;
+// int OrbitalMotorSteps     = 0;
  
 void setup() {
   lcd.begin();
@@ -86,25 +87,18 @@ void setup() {
   Linear_ButtonState    = digitalRead(Linear_ButtonPin);
   Limit_Switch_OneState = digitalRead(Limit_Switch_OnePin);
   Limit_Switch_TwoState = digitalRead(Limit_Switch_TwoPin);
+  Speed_PontentiometerState = analogRead(A0);
  
     // While loop for horizontal homing.
-  while (Limit_Switch_TwoState != HIGH && Limit_Swtich_OneState != HIGH) {  // While the limit switches are not closed at the same time (should never happen),
-    int Speed_PontentiometerState = analogRead(A0)                          // Read the potentiometer input
-    LinearMotorSpeed = map(Speed_PotentiometerState, 0, 1023, 0, 350)       // Map voltages to rpm ****NEEDS TO BE VERIFIED****
-    if (LinearMotorSpeed > 0) {                                             // If potentiometer reading is greater than zero,
-      LinearStepper.setSpeed(LinearMotorSpeed);                             // Set linear motor speed to potentiometer input,
-      LinearStepper.step(LinearMotorStepPerRev/100);                        // Send step command. ****NEEDS TO BE VERIFIED****
+  while (Limit_Switch_TwoState != HIGH) {                                   // While limit switch one is open,
+      moderateSpeed = 20;                                                   // Set moderate motor speed
+      LinearStepper.setSpeed(-moderateSpeed);                               // Set linear motor speed to potentiometer input,
+      LinearStepper.step(LinearMotorStepPerRev/100);                        // Send step command. **** DIRECTION NEEDS TO BE VERIFIED****
     }
-    if (Limit_Swtich_OneState == HIGH) {                                     // If limit switch one is closed,
-      LinearStepper.setSpeed(-LinearMotorSpeed);                            // Change the direction of the motor
-      LinearMotorSteps = stepCount;                                         // Define number of steps the linear motor takes as the step count
-      stepCount = 0;                                                        // Set initial value of steps
-      LinearStepper.step(LinearMotorStepPerRev/100);                        // Send step command. ****NEEDS TO BE VERIFIED****
-    }
-    if (Limit_Switch_TwoState == HIGH) {                                     // If limit switch two is closed,
-      NumberOfSteps = LinearMotorSteps/2;                                   // Divide number of steps taken from limit switch one to limit switch two by two,
+    if (Limit_Switch_TwoState == HIGH) {                                    // If limit switch two is closed, (while loop ends and thus, motor should stop stepping
+      NumberOfSteps = 50;                                                   // Set number of steps to the halfway point (as measured through computation/test),
       LinearStepper.setSpeed(LinearMotorSpeed);                             // Change motor direction,
-      LinearStepper.step(NumberOfSteps/100);                                // Send step command, this should send the motor to halfway between the limit switches, i.e., the middle of the rail.
+      LinearStepper.step(50);                                               // Send step command, this should send the motor to halfway between the limit switches, i.e., the middle of the rail.
     }
   }
   
